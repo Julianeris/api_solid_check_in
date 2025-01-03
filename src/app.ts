13beb5527b0562c8506bf967b1a745d1b09721d4
Julia Lima <julianlima@outlook.com>
2025-01-03 08:32:38 -0300

    configure prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import { PrismaClient } from "@prisma/client";
 import fastfy from "fastify";
 
 export const app = fastfy()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data:{
+    name: 'John Doe',
+    email: 'email@email.com'
+  },
+})
